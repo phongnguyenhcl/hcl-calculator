5ffc23d895dd5f39d99cd036ddef838dfb21e817
Phong Van Nguyen <phong_nguyen@hcl.com>
2020-12-21 17:40:30 -0500

    add looping functionality

--- p1-calculator/src/calculator/CalculatorPlayground.java
+++ p1-calculator/src/calculator/CalculatorPlayground.java
@@ -14,28 +14,38 @@ import java.lang.*;
 public class CalculatorPlayground {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter The First Number: ");
-		double num1 = scan.nextDouble();
-		System.out.println("Enter The Second Number: ");
-		double num2 = scan.nextDouble();
-		System.out.println("Choose an operator +, -, *, /: ");
-		scan.nextLine();
-		String operator = scan.nextLine();
+		double num1;
+		double num2;
+		String operator;
+		Calculator c1;
 		
-		if (num2 == 0 && operator.equals("/")) {
-			System.out.println("Cannot divide by 0. Please try again");
-		} else {
-			Calculator c1 = new Calculator(num1, num2, operator);
+		Scanner scan = new Scanner(System.in);
+		
+		String use = "yes";
+		while (use.equals("yes")) {
+			System.out.println("Enter The First Number: ");
+			num1 = scan.nextDouble();
+			System.out.println("Enter The Second Number: ");
+			num2 = scan.nextDouble();
+			System.out.println("Choose an operator +, -, *, /: ");
+			scan.nextLine();
+			operator = scan.nextLine();
 			
-			if (c1.getResult() % 1 != 0) {
-				System.out.println(String.format("%.3f", c1.getResult()));
+			c1 = new Calculator(num1, num2, operator);
+			if (num2 == 0 && operator.equals("/")) {
+				System.out.println("Cannot divide by 0. Please try again");
 			} else {
-				System.out.println(String.format("%.0f", c1.getResult()));
-
+				if (c1.getResult() % 1 != 0) {
+					System.out.println(String.format("%.3f", c1.getResult()));
+				} else {
+					System.out.println(String.format("%.0f", c1.getResult()));
+				}
 			}
-			System.out.println();
-			System.out.println("Number of Calculators: " + Calculator.calculatorCount);
+			System.out.println("type yes to continue: " );
+			use = scan.nextLine();
 		}
+		System.out.println();
+		System.out.println("Done!");
+		System.out.println("Number of Calculators: " + Calculator.calculatorCount);
 	}
 }
